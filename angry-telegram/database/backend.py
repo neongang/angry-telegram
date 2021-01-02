5dd43fdb348f1192f7fbd22c92f468c1caf6dad7
pqhaz <me@pqhaz.dev>
2021-01-02 12:24:07 +0100

    Use other chat names for db
    bye bye ftg users
    
    Signed-off-by: pqhaz <me@pqhaz.dev>

--- angry-telegram/database/backend.py
+++ angry-telegram/database/backend.py
@@ -48,7 +48,7 @@ class CloudBackend():
 
     async def _find_data_channel(self):
         async for dialog in self._client.iter_dialogs(None, ignore_migrated=True):
-            if dialog.name == f"friendly-{self._me.user_id}-data" and dialog.is_channel:
+            if dialog.name == f"angry-{self._me.user_id}-data" and dialog.is_channel:
                 members = await self._client.get_participants(dialog, limit=2)
                 if len(members) != 1:
                     continue
@@ -59,14 +59,14 @@ class CloudBackend():
         async with self._anti_double_lock:
             if not self._data_already_exists:
                 self._data_already_exists = True
-                return (await self._client(CreateChannelRequest(f"friendly-{self._me.user_id}-data",
+                return (await self._client(CreateChannelRequest(f"angry-{self._me.user_id}-data",
                                                                 "// Don't touch", megagroup=True))).chats[0]
             else:
                 return await self._find_data_channel()
 
     async def _find_asset_channel(self):
         async for dialog in self._client.iter_dialogs(None, ignore_migrated=True):
-            if dialog.name == f"friendly-{self._me.user_id}-assets" and dialog.is_channel:
+            if dialog.name == f"angry-{self._me.user_id}-assets" and dialog.is_channel:
                 members = await self._client.get_participants(dialog, limit=2)
                 if len(members) != 1:
                     continue
@@ -77,7 +77,7 @@ class CloudBackend():
         async with self._anti_double_asset_lock:
             if not self._assets_already_exists:
                 self._assets_already_exists = True
-                return (await self._client(CreateChannelRequest(f"friendly-{self._me.user_id}-assets",
+                return (await self._client(CreateChannelRequest(f"angry-{self._me.user_id}-assets",
                                                                 "// Don't touch", megagroup=True))).chats[0]
             else:
                 return await self._find_data_channel()
